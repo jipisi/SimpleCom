c1a8fd1dba3dec3ea9417b44c88513a34f8e0c9e
Lucas Lu(卢佳) <lujia3@founderpcb.com>
2017-11-14 14:56:10 +0800

    changes log

---
+++ MyFirstCom/MyFirstCom/ComClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace MyFirstCom
+{
+    [Guid("DEA10720-E600-4712-8A92-411B4E77D0FD"),ClassInterface(ClassInterfaceType.None),ComSourceInterfaces(typeof(MyCom_Events))]
+    public class ComClass : MyCom_Interface
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
---
+++ MyFirstCom/MyFirstCom/ComInterface.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace MyFirstCom
+{
+    [Guid("DF884273-95A1-410d-8CD5-0B69D610BBAF")]
+    public interface MyCom_Interface
+    {
+        [DispId(1)]
+        int Add(int a, int b);
+    }
+
+    [Guid("C1942A47-7967-4648-B531-F3C694D442BE"), InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    public interface MyCom_Events
+    {
+
+    }
+
+
+}
